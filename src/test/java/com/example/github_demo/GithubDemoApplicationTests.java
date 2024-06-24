840776b101f41059d25fc79df72a21af895b71e2
vigneshgj <gjvignesh.devops@gmail.com>
2024-06-24 14:29:09 +0530

    Added maven war files

---
+++ src/test/java/com/example/github_demo/GithubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
